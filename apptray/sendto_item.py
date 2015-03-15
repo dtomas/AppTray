2d719e9d5ad45b280a3d8b3c0be92fb9443f22e0
Dennis Tomas <dtomas@onegreenbyte.com>
2015-03-15 21:43:10 +0100

    Install RunActions.

--- apptray/sendto_item.py
+++ apptray/sendto_item.py
@@ -2,9 +2,15 @@ import os
 from traylib import XDG_CONFIG_HOME
 
 _sendto_dir = os.path.join(XDG_CONFIG_HOME, 'rox.sourceforge.net', 'SendTo')
+_runactions_dir = os.path.join(
+    XDG_CONFIG_HOME, 'rox.sourceforge.net', 'MIME-types'
+)
 
-if not os.path.isdir(_sendto_dir):
+if not os.path.exists(_sendto_dir):
     os.makedirs(_sendto_dir)
+
+if not os.path.exists(_runactions_dir):
+    os.makedirs(_runactions_dir)
 
 class SendToItemNotCreated(Exception):
     pass
@@ -14,7 +20,8 @@ class SendToItem:
     
     def __init__(self, app, mime_type):
         assert mime_type
-        sendto_dir = os.path.join(_sendto_dir, '.%s' % mime_type.replace('/', '_'))
+        filename = mime_type.replace('/', '_')
+        sendto_dir = os.path.join(_sendto_dir, '.%s' % filename)
         if not os.path.isdir(sendto_dir):
             try:
                 os.makedirs(sendto_dir)
@@ -26,6 +33,10 @@ class SendToItem:
             os.symlink(app.dnd_path, self.__path)
         except OSError:
             raise SendToItemNotCreated
+
+        runaction_path = os.path.join(_runactions_dir, filename)
+        if not os.path.exists(runaction_path):
+            os.symlink(self.__path, runaction_path)
 
     def remove(self):
         os.remove(self.__path)

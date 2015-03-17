83cefbf2811a2d834b8c062b76c5717ac33af174
Dennis Tomas <dtomas@onegreenbyte.com>
2015-03-17 22:47:59 +0100

    Watch directories in APPDIRPATH for changes.

--- apptray/handlers/rox/rox_app_handler.py
+++ apptray/handlers/rox/rox_app_handler.py
@@ -1,6 +1,6 @@
 import os
 
-from traylib import APPDIRPATH
+from traylib import APPDIRPATH, dir_monitor
 
 from apptray.app_handler import AppHandler
 from apptray.handlers.rox.rox_app import RoxApp, NotAnAppDir
@@ -12,17 +12,34 @@ class RoxAppHandler(AppHandler):
         self.__app_added = app_added
         self.__app_removed = app_removed
 
+        self.__apps = {}
+
+    def _monitor_apps_dir(self, apps_dir):
+        dir_monitor.add(apps_dir, self)
+        # FIXME: Wanna watch subdirectories, but this makes the dir monitor hang
+        # when there are many of them. Is this some gamin/inotify limitation?
+        #for dirname in os.listdir(apps_dir):
+        #    path = os.path.join(apps_dir, dirname)
+        #    if not os.path.isdir(path):
+        #        continue
+        #    if path in self.__apps:
+        #        continue
+        #    #self._monitor_apps_dir(path)
+
     def _get_apps(self, apps_dir):
         for app_dir in os.listdir(apps_dir):
             app_path = os.path.join(apps_dir, app_dir)
             if not os.path.isdir(app_path):
                 continue
             try:
-                yield RoxApp(self, app_path)
+                app = RoxApp(self, app_path)
             except NotAnAppDir:
                 # Not an app dir, recurse...
                 for app in self._get_apps(app_path):
                     yield app
+            else:
+                self.__apps[app_path] = app
+                yield app
 
     def __iter__(self):
         for apps_dir in APPDIRPATH:
@@ -30,3 +47,29 @@ class RoxAppHandler(AppHandler):
                 continue
             for app in self._get_apps(apps_dir):
                 yield app
+
+        for apps_dir in APPDIRPATH:
+            if not os.path.isdir(apps_dir):
+                continue
+            self._monitor_apps_dir(apps_dir)
+
+    def file_created(self, dir, leaf):
+        path = os.path.join(dir, leaf)
+        if not os.path.isdir(path):
+            return
+        try:
+            app = RoxApp(self, path)
+        except NotAnAppDir:
+            dir_monitor.add(path, self)
+        else:
+            self.__apps[path] = app
+            self.__app_added(app)
+
+    def file_deleted(self, dir, leaf):
+        path = os.path.join(dir, leaf)
+        try:
+            app = self.__apps[path]
+        except KeyError:
+            return
+        print(app)
+        self.__app_removed(app)

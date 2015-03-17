c521fccf965551a4cd06a95cee6c23dd3e8693a4
Dennis Tomas <dtomas@onegreenbyte.com>
2015-03-17 13:23:22 +0100

    Documented AppHandler base class.

--- apptray/app_handler.py
+++ apptray/app_handler.py
@@ -2,7 +2,13 @@
 class AppHandler:
     
     def __iter__(self):
+        """
+        Get all available applications.
+
+        @return: list of L{apptray.app.App}
+            The list of applications.
+        """
         raise NotImplementedError
 
     def handle_uri(self, uri):
-        pass
+        """Handle a URI dragged to the main icon."""
